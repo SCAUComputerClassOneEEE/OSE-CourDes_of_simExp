63f284f1835b431a21c2fb1f32615f507deffc8a
hiluyx <luyx27877@163.com>
2020-07-19 13:04:02 +0800

    磁盘行为基本完成，差write

--- src/main/java/com/SCAUComputerClassOneEEE/OSEC/dataOjb/diskSim/Disk.java
+++ src/main/java/com/SCAUComputerClassOneEEE/OSEC/dataOjb/diskSim/Disk.java
@@ -21,23 +21,45 @@ public class Disk {
         fat.recovery_FAT(header);
     }
 
+    /**
+     *
+     * @param str 写入的字符串
+     * @return -1表示没有足够空间
+     * @throws Exception dangerous operation
+     */
     public int malloc(String str) throws Exception {
         int strLength = str.length();
+
         int preBlockSize = (strLength % 64) > 0 ? strLength/64 + 1 : strLength/64;
         int header = fat.malloc_FAT(preBlockSize);
+        if (header == -1) return -1;
         write(header,str);
         return header;
     }
 
     public void write(int position,String str) throws Exception {
-        /*
-         两种情况，新创建或修改。
-         1.新创建时，只能通过malloc。
-         2.修改时长度变化需要改分配表（检查最后一次写的块在表中是否为-1）
-         */
         if (position == 0) throw new Exception("dangerous operation");
-        synchronized (diskBlocks[position]){
+        if (fat.getFileSize(position) > 1)
+        while (str.length() > 0){
+            String element = str.substring(0,64);
+            str = str.substring(64);
 
+            synchronized (diskBlocks[position]){
+                if (fat.getFAT_cont()[position] == -1){
+                    diskBlocks[position].w(element);
+                    int newBlock = fat.malloc_FAT(1);
+
+
+
+                }else{
+                    diskBlocks[position].w(element);
+                    position = fat.getFAT_cont()[position];
+                }
+            }
+        }
+        if (fat.getFAT_cont()[position] != -1) {
+            fat.recovery_FAT(fat.getFAT_cont()[position]);
+            fat.getFAT_cont()[position] = -1;
         }
     }
 
@@ -50,14 +72,13 @@ public class Disk {
         return stringBuffer.toString();
     }
 
-    public void markDamage(int position){
-        fat.mark_FAT(position);
-    }
+    public void markDamage(int position){ fat.mark_FAT(position); }
 
     @Setter
     @Getter
     private static class FAT{
 
+        int freeBlocks = 126;
         int frsFreePosition = 2;//记录第一个空闲块索引
         final int[] FAT_cont = new int[128];
 
@@ -76,17 +97,23 @@ public class Disk {
             synchronized (FAT_cont){
                 if (header < frsFreePosition) frsFreePosition = header;
                 FAT_cont[header] = 0;
+                /* 计算剩余空闲 */
+                freeBlocks ++;
                 recovery_FAT(FAT_cont[FAT_cont[header]]);
             }
         }
 
         /**
          * 获得文件分配表中首个空闲块
+         * -1表示已满
          * @return 首个空闲块
          */
-        int getFreeBlockOrder(){
+        private int getFreeBlockOrder(){
             int ret = frsFreePosition;
-            for (int i = frsFreePosition; i < 128; i++)
+            /* 计算剩余空闲 */
+            freeBlocks --;
+            int i;
+            for (i = frsFreePosition; i < 128; i++)
                 if (FAT_cont[i] == 0) {
                     frsFreePosition = i;
                     break;
@@ -95,21 +122,21 @@ public class Disk {
         }
 
         /**
-         * 文件分配表的分配内存
+         * 从文件分配表第一个空闲块开始分配 preBlockSize 份内存
          * @param preBlockSize 请求划分的块数
          * @return 文件块头
          */
         int malloc_FAT(int preBlockSize){
+            if (preBlockSize <= 0) return -1;
+            if (preBlockSize > freeBlocks) return -1;
             int header = 0;
             for (int i = 1,prePos = 0; i <= preBlockSize; i ++){
-                int freeOrder = fat.getFreeBlockOrder();
-                if (i ==1) {
-                    header = freeOrder;
-                }else if(i == preBlockSize){
-                    fat.getFAT_cont()[freeOrder] = -1;
-                }else{
-                    fat.getFAT_cont()[prePos] = freeOrder;
-                }
+                int freeOrder = getFreeBlockOrder();
+
+                if (i ==1) header = freeOrder;
+                else FAT_cont[prePos] = freeOrder;
+
+                if(i == preBlockSize) FAT_cont[freeOrder] = -1;
                 prePos = freeOrder;
             }
             return header;
@@ -132,12 +159,13 @@ public class Disk {
          * @return 文件块数
          */
         int getFileSize(int header){
+            if (FAT_cont[header] == 0) return 0;
             if (FAT_cont[header] == -1) return 1;
             else return getFileSize(FAT_cont[FAT_cont[header]]) + 1;
         }
 
         /**
-         * 获取文件流串的索引
+         * 获取文件流串的索引链表
          * @param header 文件块头
          * @return 文件流串的索引
          */
@@ -169,7 +197,12 @@ public class Disk {
             }
             block_cont[cur] = newChar;
         }
-
+        void w(String newStr){
+            if (block_cont == null){
+                block_cont = new char[64];
+            }
+            block_cont = newStr.toCharArray();
+        }
         String r() {
             if (block_cont == null) return "";
             return String.copyValueOf(block_cont);
