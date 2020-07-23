9679b443cac4ff43d2d72fb70a486df0c21e841e
hiluyx <luyx27877@163.com>
2020-07-23 17:35:21 +0800

    磁盘代码写完，待测试！

--- src/main/java/com/SCAUComputerClassOneEEE/OSEC/simExpApp.java
+++ src/main/java/com/SCAUComputerClassOneEEE/OSEC/simExpApp.java
@@ -1,7 +1,19 @@
 package com.SCAUComputerClassOneEEE.OSEC;
 
+import com.SCAUComputerClassOneEEE.OSEC.utils.TaskThreadPools;
+
 public class simExpApp {
     public static void main(String[] args) {
-
+        String str = "TaskThreadPools_TaskThreadPools_TaskThreadPools_TaskThreadPools_TaskThreadPools_TaskThreadPools_1234";
+        int preBlockSize = str.length()/32;
+        for (int i = 0; i < preBlockSize + 1; i ++){
+            System.out.println(i);
+            String wStr;
+            if (i != preBlockSize) wStr = str.substring(i*32, i*32+32);
+            else {
+                wStr = str.substring(i * 32);
+            }
+            System.out.println(wStr);
+        }
     }
 }
