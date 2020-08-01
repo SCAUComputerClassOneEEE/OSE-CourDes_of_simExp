3efac4343f3622ed50cc2a953db8c9233a77bb44
sky <mad_sky_scau@163.com>
2020-08-01 19:48:15 +0800

    8月1会议

--- src/main/java/com/SCAUComputerClassOneEEE/OSEC/dataService/SimulationDataService.java
+++ src/main/java/com/SCAUComputerClassOneEEE/OSEC/dataService/SimulationDataService.java
@@ -1,5 +1,4 @@
 package com.SCAUComputerClassOneEEE.OSEC.dataService;
 
 public interface SimulationDataService {
-    void fangfa();
 }
--- src/main/java/com/SCAUComputerClassOneEEE/OSEC/dataService/impl/DeviceSimService.java
+++ src/main/java/com/SCAUComputerClassOneEEE/OSEC/dataService/impl/DeviceSimService.java
@@ -3,9 +3,4 @@ package com.SCAUComputerClassOneEEE.OSEC.dataService.impl;
 import com.SCAUComputerClassOneEEE.OSEC.dataService.SimulationDataService;
 
 public class DeviceSimService implements SimulationDataService {
-
-    @Override
-    public void fangfa() {
-        return;
-    }
 }
