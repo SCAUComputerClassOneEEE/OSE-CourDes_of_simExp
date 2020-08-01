20eb64b65292053e080aebef4cd51d1806697935
jh <1258740087@qq.com>
2020-08-01 19:03:23 +0800

    命令行输入提交

--- src/main/java/com/SCAUComputerClassOneEEE/OSEC/op/Terminal.java
+++ src/main/java/com/SCAUComputerClassOneEEE/OSEC/op/Terminal.java
@@ -10,13 +10,17 @@ import javafx.stage.Stage;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.SCAUComputerClassOneEEE.OSEC.dataOjb.diskSim.FileTree;
+
 /**
  * @author hlf
  * @date 28/7/2020
  */
 public class Terminal {
+    //格式串
     public Pattern pattern1 = Pattern.compile("([a-zA-Z]+)\\s([/,\\w]+)");
     public Pattern pattern2 = Pattern.compile("([a-zA-Z]+)\\s([/,\\w]+)\\s([/,\\w]+)");
+    //输入区
     public static TextArea textArea = new TextArea();
 
 
@@ -25,20 +29,25 @@ public class Terminal {
 
         textArea.appendText(">> ");
         final int[] currentCaretPos = {textArea.getCaretPosition()};
+        //设置到不可编辑与可编辑区域
         textArea.setOnKeyPressed(event -> {
 
             if (event.getCode() == KeyCode.BACK_SPACE && currentCaretPos[0] == textArea.getCaretPosition()) {
+                //当按下删除并且到此时距离等于最小距离时，不可编辑
                 textArea.setEditable(false);
             } else {
                 textArea.setEditable(true);
             }
         });
+        //当按键松开后触发
         textArea.addEventHandler(KeyEvent.KEY_RELEASED, new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-
+                //当按下回车时
                 if (event.getCode() == KeyCode.ENTER) {
+                    //获取执行的语句
                     String command = textArea.getText(currentCaretPos[0], textArea.getCaretPosition() - 1);
+                    //光标的位置，用于之前是否可编辑操作
                     currentCaretPos[0] = textArea.getCaretPosition() + 3;
                     textArea.appendText(">> ");
                     textArea.positionCaret(textArea.getCaretPosition());
@@ -53,6 +62,7 @@ public class Terminal {
 
 
     public void formatStr(String command) {
+        //去掉多余空格
         String arr = command.replaceAll("\\s+", " ");
         //匹配模式
         Matcher matcher1 = pattern1.matcher(arr);
@@ -60,8 +70,13 @@ public class Terminal {
 
         if (matcher1.matches()) {
             System.out.println(matcher1.group(0));
-            String action = matcher1.group(1);
-            String filePath = matcher1.group(2);
+            String action = matcher1.group(1); //获取动作
+            String filePath = matcher1.group(2);//获取路径
+
+            switch(action){
+                case "create":
+
+            }
             //下面是文件方法调用
 
 //            switch (action) {
@@ -100,8 +115,8 @@ public class Terminal {
 //            }
 //
         } else if (matcher2.matches()) {
-            String srcFilepath = matcher2.group(2);
-            String desFilePath = matcher2.group(3);
+            String srcFilepath = matcher2.group(2); //源文件路径
+            String desFilePath = matcher2.group(3);//目标文件路径
 //            if(fileOperations.copy(srcFilepath,desFilePath)){
 //                textArea.appendText("复制成功！");
 //            }else{
