9b269b91386ca3c1a10f2ea0524211891940db67
jh <1258740087@qq.com>
2020-08-04 13:27:03 +0800

    8/4 13.26

--- src/main/java/com/SCAUComputerClassOneEEE/OSEC/op/Terminal.java
+++ src/main/java/com/SCAUComputerClassOneEEE/OSEC/op/Terminal.java
@@ -11,6 +11,9 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -34,9 +37,7 @@ public class Terminal {
         this.disk = disk;
         this.fileTree = fileTree;
         this.rootTree = fileTree.getRootTree();
-//备用，文件名查找函数
-//        ObservableList<TreeItem<AFile>> myTreeItems = rootTree.getChildren();
-//        myTreeItems.get(0).getValue().getFileName();
+
 
         String command = "";
 
@@ -79,13 +80,42 @@ public class Terminal {
             System.out.println(matcher1.group(0));
             String action = matcher1.group(1);
             String filePath = matcher1.group(2);
+            List<String> fileNameList = getfileNameList(filePath);
 
             switch (action){
                 case "create":
-                    textArea.appendText(tib.createFile(disk, rootTree, filePath));
+                    for (int i = 0; i < fileNameList.size(); i++){
+                        if (i == 0 && fileNameList.size() == 1){
+                            textArea.appendText(tib.createFile(disk, rootTree, fileNameList.get(i)));
+                        }else if(i == 1){
+                            ObservableList<TreeItem<AFile>> myTreeItems = rootTree.getChildren();
+                            for (int j = 0; j < myTreeItems.size(); j++){
+                                if(fileNameList.get(i - 1).equals(myTreeItems.get(j).getValue().getFileName())){
+                                    textArea.appendText(tib.createFile(disk, myTreeItems.get(j), fileNameList.get(i)));
+                                }
+                            }
+                        }
+                    }
+
+
                     break;
                 case "mkdir":
-                    textArea.appendText(tib.createDirectory(disk, rootTree, filePath));
+                    //注释的方法正常
+//                    for (int i = 0; i < fileNameList.size(); i++){
+//                        if (i == 0 && fileNameList.size() == 1){
+//                            textArea.appendText(tib.createDirectory(disk, rootTree, fileNameList.get(i)));
+//                        }else if(i == 1){
+//                            ObservableList<TreeItem<AFile>> myTreeItems = rootTree.getChildren();
+//                            for (int j = 0; j < myTreeItems.size(); j++){
+//                                if(fileNameList.get(i - 1).equals(myTreeItems.get(j).getValue().getFileName())){
+//                                    textArea.appendText(tib.createDirectory(disk, myTreeItems.get(j), fileNameList.get(i)));
+//                                }
+//                            }
+//                        }
+//                    }
+                    textArea.appendText(tib.createDirectory(disk, getFatherTreeItem(fileNameList, rootTree, 0),
+                            fileNameList.get(fileNameList.size() - 1)));
+
                     break;
 //                case "delete":
 //                    if (tib.delete(disk, rootTree)){
@@ -150,4 +180,58 @@ public class Terminal {
 
         }
     }
+
+    /**
+     *
+     * @param filePath
+     * @return 文件名数组，每个元素都为上一个元素所属TreeItem的子的文件名
+     */
+
+    List<String> getfileNameList(String filePath){
+        //将字符串以 / 拆分成几个字符串，其中fileNames[0]是第一个 / 前面的字符，如果第一个字符是 / ，则fileNames[0]为空(可打印
+        String[] fileNames = filePath.split("/");
+        //将数组第一个空内容去掉,并转化成List类型
+        List<String> fileNameList = new ArrayList<String>(Arrays.asList(fileNames));
+        fileNameList.remove(0);
+//解开注释证明从下标 0 开始
+//            for(int i = 0; i < fileNameList.size(); i++){
+//                System.out.println(fileNameList.get(i));
+//
+//            }
+        return fileNameList;
+
+    }
+
+    /**
+     * 递归找到fileNameList对应的父节点
+     * @param fileNameList
+     * @param fatherTreeItem
+     * @return
+     */
+    TreeItem<AFile> getFatherTreeItem(List<String> fileNameList, TreeItem<AFile> fatherTreeItem, int i){
+        textArea.appendText(fatherTreeItem.toString());
+        // i从0开始,务必让 i 初值为0
+        if(fileNameList.size() == 1){
+            return rootTree;
+        }else{
+//            if(i == (fileNameList.size() - 1)){
+//                return fatherTreeItem;
+//            }
+            ObservableList<TreeItem<AFile>> myTreeItems = fatherTreeItem.getChildren();
+            if(myTreeItems.isEmpty()) { return fatherTreeItem; }
+            for (int j = 0; j < myTreeItems.size(); j++){
+                if(fileNameList.get(i).equals(myTreeItems.get(j).getValue().getFileName())){
+                    i++;
+                    getFatherTreeItem(fileNameList, myTreeItems.get(j), i);
+                }
+                //没找到,循环结束后 j = myTreeItems.size()  退出条件
+//                if(j == (myTreeItems.size() - 1)){
+//                    textArea.appendText("father not found");
+//                    return fatherTreeItem;
+//                }
+            }
+        }
+        return null;
+    }
+
 }
