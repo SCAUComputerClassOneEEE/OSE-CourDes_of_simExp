cf2557e0fc0c8645b449cf90c0f00d429a0a6924
hiluyx <luyx27877@163.com>
2020-08-26 08:51:15 +0800

    disk自测完毕

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,7 +1,26 @@
+import com.SCAUComputerClassOneEEE.OSEC.dataOjb.diskSim.Disk;
 import com.SCAUComputerClassOneEEE.OSEC.dataOjb.storageSim.MEM.Memory;
 
 public class Test {
     public static void main(String[] args) throws Exception {
+        Disk testDisk = new Disk();
+        int header1 = testDisk.malloc_F_Header();
+        testDisk.writeFile(header1,"header1 wrote.");
+        int header2 = testDisk.malloc_F_Header();
+        testDisk.writeFile(header2,"header2 wrote.qwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwer");
+        int header3 = testDisk.malloc_F_Header();
+        testDisk.writeFile(header3,"header3 wrote.");
+        testDisk.writeFile(header2,"header2 wrote.qwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwerqwer");
+
+    }
+    public void diskTest() throws Exception {
+        Disk testDisk = new Disk();
+        int header1 = testDisk.malloc_F_Header();
+        testDisk.writeFile(header1,"header1 wrote.");
+        int header2 = testDisk.malloc_F_Header();
+        testDisk.writeFile(header2,"header2 wrote.");
+    }
+    public void memoryTest() throws Exception {
         Memory memoryTest = new Memory();
         System.out.println("return process pointer " + memoryTest.malloc(10, "1234567890"));
         System.out.println("return process pointer " + memoryTest.malloc(10, "1234567890"));
