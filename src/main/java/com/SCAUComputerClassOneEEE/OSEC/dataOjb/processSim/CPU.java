893c67b950a815754bc7df0759ee34f00f594634
helefeng <53102304+helefeng@users.noreply.github.com>
2020-10-11 15:23:54 +0800

    完成编译器

--- src/main/java/com/SCAUComputerClassOneEEE/OSEC/dataOjb/processSim/CPU.java
+++ src/main/java/com/SCAUComputerClassOneEEE/OSEC/dataOjb/processSim/CPU.java
@@ -1,6 +1,7 @@
 package com.SCAUComputerClassOneEEE.OSEC.dataOjb.processSim;
 
 import com.SCAUComputerClassOneEEE.OSEC.dataOjb.diskSim.FileModel.AFile;
+import com.sun.tools.javac.util.Convert;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -20,12 +21,13 @@ public class CPU {
     public static Pattern format1 =Pattern.compile("(^[a-zA-Z]+)(\\++|--)");//匹配自（加/减）
     public static Matcher matcher1;
 
-    public static Pattern format2 =Pattern.compile("!([A|B|C])(\\d{1,2})");
+    public static Pattern format2 =Pattern.compile("!([A|B|C])(\\d{1,2})");//匹配设备
     public static Matcher matcher2;
 
     public static String format3 ="end";
     public static Pattern format4 =Pattern.compile("(^[a-zA-Z]+)=(\\d{1,2})"); //匹配赋值语句
     public static Matcher matcher4;
+
 
     private int psw=0;//程序状态字
 
@@ -67,7 +69,9 @@ public class CPU {
         create(executeFile);//创建进程
 
         //执行指令部分
+        for(int i=0;i<6;i++){
 
+        }
 
     }
 
@@ -114,14 +118,36 @@ public class CPU {
     private void initExeFile(){
 
     }
+    public static StringBuffer decompile(String compliedResult){
+        /**
+         *  8个bit，前三位为操作码
+         *  000自加，001自减，后面全为0
+         *  010为申请设备，00为A设备，01为B设备，10为C设备，后3位为使用时间
+         *  011为赋值,后面为赋值数据
+         *  100为end，后面全为0
+         */
 
+        String op = compliedResult.substring(0,2);
+        switch (op){
+            case "000":
+            case "001":
+                break;
+            case "100":
+                break;
+            case "010":
+                break;
+            case "011":
+                break;
+        }
+        return null;
+    }
     public static void main(String[] args) {
         File file = new File("/Users/apple/Desktop/test.txt");
         ArrayList<String> contents = new ArrayList<>();
         type(file,contents);
         for(String each:contents){
             System.out.println(each);
-
+            String compiledResult = null;
             matcher1 = format1.matcher(each);
             if(matcher1.matches()){
                 String name = matcher1.group(1);
@@ -129,32 +155,73 @@ public class CPU {
                 int value = map.get(name);
                 if(action.equals("++")){
                     value++;
+                    compiledResult="00000000";
+                    System.out.println("这是自加，编译结果为："+compiledResult);
                 }else{
                     value--;
+                    compiledResult="00100000";
+                    System.out.println("这是自减，编译结果为："+compiledResult);
                 }
                 PC++;
                 map.put(name,value);
                 System.out.println(name+":"+value);
+
             }
 
             matcher2 = format2.matcher(each);
             if(matcher2.matches()){
+                compiledResult="010";
                 String deviceName = matcher2.group(1);
+                switch (deviceName){
+                    case "A":compiledResult+=00;
+                    break;
+                    case "B":compiledResult+=01;
+                    break;
+                    case "C":compiledResult+=10;
+                    break;
+                }
                 int time = Integer.parseInt(matcher2.group(2));
                 System.out.println(deviceName+":"+time);
+
+                String result = Integer.toBinaryString(time);
+                StringBuffer s = new StringBuffer();
+                for(int i=result.length();i<=3;i++){
+                    s.append("0");
+                }
+                s.append(result);
+                result = s.toString();
+
+                compiledResult+=result;
+                System.out.println(result);
+                System.out.println("这是设备申请，编译结果为："+compiledResult);
                 PC++;
             }
 
             if(each.matches(format3)){
+                compiledResult="10000000";
                 PC++;
+                System.out.println("这是结束，编译结果为："+compiledResult);
             }
 
             matcher4 = format4.matcher(each);
             if(matcher4.matches()){
+                compiledResult = "011";
                 String name = matcher4.group(1);
                 int num = Integer.parseInt(matcher4.group(2));
+
+                String result = Integer.toBinaryString(num);
+                StringBuffer s = new StringBuffer();
+                for(int i=result.length();i<5;i++){
+                    s.append("0");
+                }
+                s.append(result);
+                result = s.toString();
+
+                compiledResult+=result;
+
                 map.put(name,num);
                 PC++;
+                System.out.println("这是赋值，编译结果为："+compiledResult);
             }
             System.out.println("正在执行第"+PC+"条指令");
             System.out.println("-------");
---
+++ src/main/java/com/SCAUComputerClassOneEEE/OSEC/dataOjb/processSim/Compile.java
@@ -0,0 +1,109 @@
+package com.SCAUComputerClassOneEEE.OSEC.dataOjb.processSim;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author zhl、hlf
+ * @date 11/10/2020
+ */
+public class Compile {
+    public static String decompile(char result) {
+        // 然后通过 Integer 中的 toBinaryString 方法来一个一个转
+        String binStr = Integer.toBinaryString(result);
+        StringBuffer s = new StringBuffer();
+        for (int j = binStr.length(); j < 8; j++) {
+            s.append("0");
+        }
+        s.append(binStr);
+        String r = s.toString();
+        /**
+         *  8个bit，前三位为操作码
+         *  000自加，001自减，后面全为0
+         *  010为申请设备，00为A设备，01为B设备，10为C设备，后3位为使用时间
+         *  011为赋值,后面为赋值数据
+         *  100为end，后面全为0
+         */
+
+        String op = r.substring(0, 3);
+        switch (op) {
+            case "000":
+                return "X++";
+            case "001":
+                return "X--";
+            case "100":
+                return "end";
+            case "010":
+                String part1 = r.substring(3, 5);
+                char equipment = (char) (B2D(part1) + 65);
+                String part2 = r.substring(5);
+                int time = B2D(part2);
+                return "!" + equipment + time;
+            case "011":
+                int decimal = B2D(r.substring(3));
+                return "X=" + decimal;
+        }
+        return null;
+    }
+
+    public static String compile(String result) {
+        int machineCode = 0;
+        StringBuilder contents = new StringBuilder();
+        Pattern format1 = Pattern.compile("(^[a-zA-Z]+)(\\++|--)");//匹配自（加/减）
+        Matcher matcher1;
+
+        Pattern format2 = Pattern.compile("!([A|B|C])(\\d{1,2})");//申请设备
+        Matcher matcher2;
+
+        String format3 = "end";
+
+        Pattern format4 = Pattern.compile("(^[a-zA-Z]+)=(\\d{1,2})"); //匹配赋值语句
+        Matcher matcher4;
+        while (result != "" && result.indexOf(";") != -1) {
+            String b = result.substring(0, result.indexOf(";"));
+            result = result.substring(result.indexOf(";") + 1);
+
+            matcher1 = format1.matcher(b);    //自（加/减）
+            if (matcher1.matches()) {
+                String action = matcher1.group(2);
+                System.out.println(matcher1.group(2));
+                if ("++".equals(action)) {
+                    contents.append((char) 0);
+                } else {
+                    contents.append((char) 32);
+
+                }
+            }
+
+            matcher2 = format2.matcher(b);    //申请设备
+            if (matcher2.matches()) {
+                String deviceName = matcher2.group(1);
+                int deviceCode = deviceName.charAt(0) - 65;
+                machineCode = Integer.parseInt(matcher2.group(2)) + 64 + deviceCode * 8;
+                contents.append((char) machineCode);
+            }
+
+            if (b.matches(format3)) {      //end
+                contents.append((char) 128);
+            }
+
+            matcher4 = format4.matcher(b);    //赋值语句
+            if (matcher4.matches()) {
+                int num = Integer.parseInt(matcher4.group(2));
+                contents.append((char) (num + 96));
+            }
+        }
+        return contents.toString();
+    }
+
+    private static int B2D(String binary) {
+        int decimal = (int) Long.parseLong(binary, 2);
+        return decimal;
+    }
+
+    public static void main(String[] args) {
+        String result = Compile.decompile(Compile.compile("X=6;").charAt(0));
+        System.out.println(result);
+
+    }
+}
