df968f3909ca0504f381ad51f0152f8795277f72
sky <mad_sky_scau@163.com>
2020-10-21 10:00:24 +0800

    2020/10/21 小优化

--- src/main/java/com/SCAUComputerClassOneEEE/OSEC/Main.java
+++ src/main/java/com/SCAUComputerClassOneEEE/OSEC/Main.java
@@ -29,6 +29,7 @@ public class Main extends Application {
             Scene scene = new Scene(root);
             //scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
             stage.setScene(scene);
+            stage.resizableProperty().setValue(Boolean.FALSE);
             stage.show();
         } catch(Exception e) {
             e.printStackTrace();
