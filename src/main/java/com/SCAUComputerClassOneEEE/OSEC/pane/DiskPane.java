44569f8db784378cb2968d4e67b7bc9c48434095
SCAUBai <13512742057@sina.cn>
2020-10-31 22:16:28 +0800

    1

--- src/main/java/com/SCAUComputerClassOneEEE/OSEC/pane/DiskPane.java
+++ src/main/java/com/SCAUComputerClassOneEEE/OSEC/pane/DiskPane.java
@@ -1,5 +1,6 @@
 package com.SCAUComputerClassOneEEE.OSEC.pane;
 
+import com.SCAUComputerClassOneEEE.OSEC.dataModel.diskSim.Disk;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
@@ -42,6 +43,13 @@ public class DiskPane {
     }
 
     public static DiskPane getDiskPane(){
+        if (diskPane == null){
+            synchronized (Disk.class){
+                if (diskPane == null){
+                    diskPane = new DiskPane();
+                }
+            }
+        }
         return diskPane;
     }
 
